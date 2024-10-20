e9f629655a3e2fc056fafb74ee415ae576d2c0a7
ZainabElSawy <zeinabhamdy377@gmail.com>
2024-10-21 00:03:19 +0300

    create payment intent model

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 
 import 'features/checkout/presentation/views/pages/my_cart_view.dart';
 
 void main() {
+  Stripe.publishableKey =
+      'pk_test_51PuwSg07toYHi9WxFOBM2SUj31IuWP5U9MW8FZYeTv0PoI5Q2lSKSLX6exulZfcshhioGT868oPpHcThgLa5btpS00ndZ3uUvW';
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
