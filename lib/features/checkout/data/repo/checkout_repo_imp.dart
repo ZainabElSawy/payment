b7b00dff2d16da4292bd6b0398944476f3c405f9
ZainabElSawy <zeinabhamdy377@gmail.com>
2024-10-21 16:50:17 +0300

    checkout repo done

---
+++ lib/features/checkout/data/repo/checkout_repo_imp.dart
@@ -0,0 +1,33 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:payment/core/errors/failure.dart';
+import 'package:payment/features/checkout/data/data_source/checkout_datasource.dart';
+import 'package:payment/features/checkout/data/models/payment_intent_input_model/payment_intent_input_model.dart';
+import 'package:payment/features/checkout/domain/repo/checkout_repo.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+
+class CheckoutRepoImp extends CheckoutRepo {
+  final CheckoutDataSource checkoutDataSource;
+  CheckoutRepoImp({required this.checkoutDataSource});
+  @override
+  Future<Either<Failure, void>> makePayment(
+      {required PaymentIntentInputModel paymentIntentInputModel}) async {
+    ConnectivityResult connectivityResult =
+        (await (Connectivity().checkConnectivity())) as ConnectivityResult;
+    if (connectivityResult != ConnectivityResult.none) {
+      try {
+        await checkoutDataSource.makePayment(
+            paymentIntentInputModel: paymentIntentInputModel);
+        return right(null);
+      } catch (e) {
+        // ignore: deprecated_member_use
+        if (e is DioError) {
+          return left(ServerFailure.fromDioError(e));
+        }
+        return left(ServerFailure(e.toString()));
+      }
+    } else {
+      return left(NetworkFailure('No internet connection'));
+    }
+  }
+}
---
+++ lib/features/checkout/domain/repo/checkout_repo.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failure.dart';
+import '../../data/models/payment_intent_input_model/payment_intent_input_model.dart';
+
+abstract class CheckoutRepo {
+  Future<Either<Failure, void>> makePayment({required PaymentIntentInputModel paymentIntentInputModel});
+}
